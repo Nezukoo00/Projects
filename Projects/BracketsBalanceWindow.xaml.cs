882e1a0f9b20804e47130db091667338ca826099
userpkcm4 <ukereng587@gmail.com>
2024-05-24 17:30:50 +0300

    new breckets ( dont work)

--- Projects/BracketsBalanceWindow.xaml.cs
+++ Projects/BracketsBalanceWindow.xaml.cs
@@ -27,21 +27,45 @@ namespace Projects
         private void CheckBracketsBalance(object sender, RoutedEventArgs e)
         {
             string input = InputTextBox.Text;
-            bool isBalanced = IsBalanced(input);
-            ResultTextBlock.Text = isBalanced ? "Balanced" : "Not Balanced";
+            
+            ResultTextBlock.Text = IsBalanced(input) ? "Balanced" : "Not Balanced";
         }
         private bool IsBalanced(string input)
-        { if (string.IsNullOrWhiteSpace(input)) return false;
+        { 
+            if (string.IsNullOrWhiteSpace(input)) return false;
+
           bool containsDigit = input.Any(char.IsDigit);
             if(!containsDigit) return false;
+            
             int balance = 0;
-            foreach (char c in input)
+            bool hasContentBetweenBrekets = false;
+
+            for (int i = 0; i < input.Length; i++)
             {
-                if (c == '(') balance++;
-                if (c == ')') balance--;
-                if (balance < 0) return false;
+                char ch = input[i];
+                if (ch ==  '(')
+                {
+                    balance++;
+                    if ( i+ 1 < input.Length && input[i+1] != ')')
+                    {
+                        hasContentBetweenBrekets = false;
+                    }
+                }
+                else if (ch == ')')
+                {
+                    balance--;
+                    if (balance < 0)
+                    {
+                        return false;
+                    }
+                }
+                else if(!char.IsWhiteSpace(ch) && balance < 0)
+                {
+                    hasContentBetweenBrekets = false;
+                }
+
             }
-            return balance == 0;
+            return balance == 0 && hasContentBetweenBrekets;
         }
         
     }
