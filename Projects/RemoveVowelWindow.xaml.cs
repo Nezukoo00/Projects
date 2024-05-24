5396e172961145f4834d409f390ca505b401bcc5
userpkcm4 <ukereng587@gmail.com>
2024-05-24 14:53:41 +0300

    remove vowel

--- Projects/RemoveVowelWindow.xaml.cs
+++ Projects/RemoveVowelWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +23,12 @@ namespace Projects
         public RemoveVowelWindow()
         {
             InitializeComponent();
+        }
+        private void RemoveVowels(object sender, RoutedEventArgs e)
+        {
+            string input = InputTextBox.Text;
+            string result = Regex.Replace(input, "[AEIOUaeiou]", "");
+            ResultTextBlock.Text = result;
         }
     }
 }
