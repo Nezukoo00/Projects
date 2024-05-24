4d684e4463fc80c5c61b2da6b11effe076be6fcc
userpkcm4 <ukereng587@gmail.com>
2024-05-24 14:32:06 +0300

    files created

---
+++ Projects/BracketsBalanceWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projects
+{
+    /// <summary>
+    /// Логика взаимодействия для BracketsBalanceWindow.xaml
+    /// </summary>
+    public partial class BracketsBalanceWindow : Window
+    {
+        public BracketsBalanceWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBracketsBalance(object sender, RoutedEventArgs e)
+        {
+            string input = InputTextBox.Text;
+            bool isBalanced = isBalanced(input);
+            ResultTextBlock.Text = isBalanced ? "Balanced" : "Not Balance"
+        }
+        private bool isBalanced(string input)
+        {
+
+        }
+    }
+}
---
+++ Projects/DoubleLettersWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Projects
+{
+    /// <summary>
+    /// Логика взаимодействия для DoubleLettersWindow.xaml
+    /// </summary>
+    public partial class DoubleLettersWindow : Window
+    {
+        public DoubleLettersWindow()
+        {
+            InitializeComponent();
+        }
+        private void FindDoubleLetters(object sender, RoutedEventArgs e)
+        {
+            string input = InputTextBox.Text;
+            var matches = Regex.Matches(input, @"(\w)\1");
+            ResultTextBlock.Text = string.Join(", ", matches);
+        }
+    }
+}
